7b62189dbd733f9ce98b828395d64e597b07e2ed
perlancar (@pc-office) <perlancar@gmail.com>
2016-07-22 02:42:34 +0700

    Initial commit

---
+++ lib/App/ValidateCPANMetaWithSah.pm
@@ -0,0 +1,17 @@
+package App::ValidateCPANMetaWithSah;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: Validate CPAN Meta specification with Sah schema
+
+=head1 SYNOPSIS
+
+See the included L<validate-cpan-meta-with-sah>.
+
+
+=head1 DESCRIPTION
+
+This is a proof-of-concept for validating CPAN Meta specification with Sah
+schema.
