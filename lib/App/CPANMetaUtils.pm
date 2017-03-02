f1897f3cb59d73cb62afee00226e54b1f048d6dd
perlancar (on pc-office) <perlancar@gmail.com>
2017-03-02 19:57:07 +0700

    Rename dist from App-ValidateCPANMetaWithSah to App-CPANMetaUtils

---
+++ lib/App/CPANMetaUtils.pm
@@ -0,0 +1,18 @@
+package App::CPANMetaUtils;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: CLI utilities related to CPAN Meta
+
+=head1 DESCRIPTION
+
+This distribution contains CLI utilities related to CPAN Meta:
+
+# INSERT_EXECS_LIST
+
+
+=head1 SEE ALSO
+
+L<CPAN::Meta>, L<CPAN::Meta::Validator>
